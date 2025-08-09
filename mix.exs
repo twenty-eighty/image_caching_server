0c8e63000832e1b0eb40f54a70557fabfc40a7d5
twenty-eighty <github.com-pareto@xn--y9a.net>
2025-08-09 09:56:23 +0200

    version update

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ImageCachingServer.MixProject do
     [
       app: :image_caching_server,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
